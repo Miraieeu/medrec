feb9db9dc9f12b448be09376b73c4019329927e0
Miraieeu <farizzarya@gmail.com>
2025-12-24 03:52:06 +0700

    Docker Compose Fix

--- dist/server.js
+++ dist/server.js
@@ -38,7 +38,7 @@ app.get("/__ping", (_req, res) => {
  * ======================
  */
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "http://localhost:4000"],
+    origin: "http://localhost:3000",
     credentials: true,
 }));
 app.use(express_1.default.json());
